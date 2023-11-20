545b7f0e5f77253d0aceaf09eae6b69cd0257a7a
daguang <daguang830@gmail.com>
2023-11-20 12:44:24 +0800

    feature: rewrite by auto memory manager

--- main.cpp
+++ main.cpp
@@ -1,8 +1,15 @@
 #include "main.h"
 
 int main() {
-  CTPMiniTrader *mApi = new CTPMiniTrader("", "", "", "");
-  mApi->~CTPMiniTrader();
+  string address = "222.66.74.18:21205";
+  string appid = "client_quantword_2.1";
+  string authCode = "WUQ2KS75AL33MX2X";
+  string brokerId = "8888";
+  string userId = "1234";
+  string password = "dlqh1552";
+
+  CTPMiniTrader *mApi = new CTPMiniTrader(address, appid, authCode, brokerId);
+  mApi->ReqAuthenticate(userId,password);
 
   return 0;
 }
--- main.h
+++ main.h
@@ -1,28 +1,28 @@
 #include <cstring>
-
+#include <memory>
 #include "include/traderapi/ThostFtdcTraderApi.h"
 #include "iostream"
 using namespace std;
 
 class CTPMiniTrader {
  private:
-  CThostFtdcTraderApi *userApi;
-  char *address;
+  CThostFtdcTraderApi *userApi = CThostFtdcTraderApi::CreateFtdcTraderApi();
+  string userId = "";
   int request = 0;
-  char *appId;
-  char *authCode;
-  char *brokerId;
-  char *userId;
+  string address;
+  string appId;
+  string authCode;
+  string brokerId;
 
  public:
-  CTPMiniTrader(char address, char appId, char authCode, char brokerId) {
-    cout << "ctp mini init" << endl;
-    cout << CThostFtdcTraderApi::GetApiVersion() << endl;
-
-    strcpy(this->address, &address);
-    strcpy(this->appId, &appId);
-    strcpy(this->authCode, &authCode);
-    strcpy(this->brokerId, &brokerId);
+  CTPMiniTrader(string address, string appId, string authCode, string brokerId)
+      : address(address),
+        appId(appId),
+        authCode(authCode),
+        brokerId(brokerId) {
+    cout << "ctp mini init, api version: "
+         << CThostFtdcTraderApi::GetApiVersion() << endl;
+    return;
   };
   ~CTPMiniTrader() {
     userApi->Release();
@@ -35,46 +35,40 @@ class CTPMiniTrader {
   // 订阅公共流
   void SubscribePublicTopic() {}
   // 客户端认证
-  int ReqAuthenticate(char *userId, char *password) {
-    this->userApi = CThostFtdcTraderApi::CreateFtdcTraderApi();
-
-    CThostFtdcReqAuthenticateField *authRequest =
+  int ReqAuthenticate(string userId, string password) {
+    unique_ptr <CThostFtdcReqAuthenticateField> authRequest =
         this->getAuthRequest(userId, password);
-    CThostFtdcReqUserLoginField *loginRequest =
+    unique_ptr <CThostFtdcReqUserLoginField> loginRequest =
         this->getUserLoginReqeust(userId, password);
 
-    int res = this->userApi->ReqAuthenticate(authRequest, this->request++);
-    printf("客户端认证 = [%d]", res);
+    int res = this->userApi->ReqAuthenticate(authRequest.get(), this->request++);
+    printf("客户端认证 = [%d] \n", res);
 
-    if (res != 0) {
-      return res;
-    }
-
-    res = this->userApi->ReqUserLogin(loginRequest, this->request++);
-    printf("用户登录 = [%d]", res);
+    res = this->userApi->ReqUserLogin(loginRequest.get(), this->request++);
+    printf("用户登录 = [%d] \n", res);
 
     return res;
   }
 
  private:
   // 生成登录结构体
-  CThostFtdcReqUserLoginField *getUserLoginReqeust(char *userId,
-                                                   char *password) {
-    CThostFtdcReqUserLoginField reqUserLogin = {0};
-    strcpy(reqUserLogin.BrokerID, this->brokerId);
-    strcpy(reqUserLogin.UserID, userId);
-    strcpy(reqUserLogin.Password, password);
-    strcpy(reqUserLogin.LoginRemark, "test");
-    return &reqUserLogin;
+  unique_ptr<CThostFtdcReqUserLoginField> getUserLoginReqeust(string userId,
+                                                   string password) {
+    unique_ptr <CThostFtdcReqUserLoginField> reqUserLogin(new CThostFtdcReqUserLoginField);
+    copy(this->brokerId.begin(), this->brokerId.end(), reqUserLogin->BrokerID);
+    copy(userId.begin(), userId.end(), reqUserLogin->UserID);
+    copy(password.begin(), password.end(), reqUserLogin->Password);
+
+    return reqUserLogin;
   }
   // 生成客户端认证结构体
-  CThostFtdcReqAuthenticateField *getAuthRequest(char *userId, char *password) {
-    this->userId = userId;
-    CThostFtdcReqAuthenticateField userRequest = {0};
-    strcpy(userRequest.BrokerID, this->brokerId);
-    strcpy(userRequest.UserID, userId);
-    strcpy(userRequest.AuthCode, this->authCode);
-    strcpy(userRequest.AppID, this->appId);
-    return &userRequest;
+  unique_ptr<CThostFtdcReqAuthenticateField> getAuthRequest(string userId,
+                                                 string password) {
+    unique_ptr <CThostFtdcReqAuthenticateField> userRequest (new CThostFtdcReqAuthenticateField());
+    copy(this->brokerId.begin(), this->brokerId.end(), userRequest->BrokerID);
+    copy(this->authCode.begin(), this->authCode.end(), userRequest->AuthCode);
+    copy(this->appId.begin(), this->appId.end(), userRequest->AppID);
+    copy(userId.begin(), userId.end(), userRequest->UserID);
+    return userRequest;
   }
 };
